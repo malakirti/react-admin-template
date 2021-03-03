faaaaa62920218f1cd422f5e38d396fa876f8150
malakirti <vimalakirti409@gmail.com>
2021-03-03 15:40:09 +0800

    chore: auto git config

--- .scripts/utils/git-config.js
+++ .scripts/utils/git-config.js
@@ -15,8 +15,15 @@ if (currentName !== name || currentEmail !== email) {
 	execSync(`git config user.name ${name}`);
 	execSync(`git config user.email ${email}`);
 
-	console.log(chalk.grey(`• current git config user.name: ${execSync('git config user.name')}`));
-	console.log(chalk.grey(`• current git config user.email: ${execSync('git config user.email')}`));
+	console.log(
+		chalk.yellowBright(`• current git config user.name: ${execSync('git config user.name')}`),
+	);
+	console.log(
+		chalk.yellowBright(`• current git config user.email: ${execSync('git config user.email')}`),
+	);
+	console.log();
 	// 在pre-commit阶段时修改，第一次需要返回错误，下一次方可commit
-	process.exit(1);
+	if (!process.env.POSTINSTALL) {
+		process.exit(1);
+	}
 }
